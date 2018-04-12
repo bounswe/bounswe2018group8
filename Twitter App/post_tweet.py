f02e2281b4a6a7c16e8492cb3c831d9fa8f721b0
Yaman Kındap <32527700+yaman-kindap@users.noreply.github.com>
2018-04-13 01:06:07 +0300

    changed message to user

--- Twitter App/post_tweet.py
+++ Twitter App/post_tweet.py
@@ -1,5 +1,5 @@
 import tweepy
 from tweepy import OAuthHandler
 
-text = input("Please enter a query. \n>>")
+text = input("Please enter your tweet: \n>>")
 new_tweet = api.update_status(status=text)
