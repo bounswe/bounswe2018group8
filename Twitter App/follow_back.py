5384c8496634e457d69e972a00c93fdc6ced14db
Yaman Kındap <32527700+yaman-kindap@users.noreply.github.com>
2018-04-13 13:49:47 +0300

    update to function format

--- Twitter App/follow_back.py
+++ Twitter App/follow_back.py
@@ -3,6 +3,7 @@ from tweepy import OAuthHandler
 import pandas as pd
 from pprint import pprint
 
-follower_ids = api.followers_ids(id=me._json['id'])
-for idx in range(len(follower_ids)):
-    api.create_friendship(id=follower_ids[idx])
+def follow_back():
+    follower_ids = api.followers_ids(id=me._json['id'])
+    for idx in range(len(follower_ids)):
+        api.create_friendship(id=follower_ids[idx])
