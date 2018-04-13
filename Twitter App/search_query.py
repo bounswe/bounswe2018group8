883680ab961777dd5c16cff625aeb1cbf881f8f9
Yaman Kındap <32527700+yaman-kindap@users.noreply.github.com>
2018-04-13 13:43:29 +0300

    update to function format

--- Twitter App/search_query.py
+++ Twitter App/search_query.py
@@ -1,8 +1,3 @@
-query = raw_input("Please enter a query. \n>>")
-
-tweets = api.search(q=query, lang='tr')
-
-tweets_dict = [tweet._json['text'] for tweet in tweets]
-for t in tweets_dict:
-	print("---------------"
-    print(t + '\n')
+def search_query(query):
+	tweets = api.search(q=query)
+	
