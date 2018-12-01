63b2cece98df655e16d86134e1a5573737db9cc0
Ozan Kınasakal <ozank@sorunapp.com>
2018-12-01 17:45:06 +0300

    Project endpoints are fixed.

--- Project/backend/project/urls.py
+++ Project/backend/project/urls.py
@@ -1,7 +1,8 @@
 # Created by Ozan Kınasakal
 from django.urls import path
-from .views import ProjectListView, ProjectManageView, ProjectCreateView
+from .views import ProjectListView, ProjectManageView, ProjectCreateView, ProjectListManageView
 
 urlpatterns = [
     path('<int:pk>/', ProjectManageView.as_view()),
+    path('', ProjectListManageView.as_view()),
 ]
--- Project/backend/project/views.py
+++ Project/backend/project/views.py
@@ -5,7 +5,7 @@ from rest_framework import authentication, permissions
 from rest_framework import generics
 from .models import Project
 from .serializers import ProjectSerializer, ProjectCreateSerializer
-from django.contrib.auth.mixins import UserPassesTestMixin
+from django.contrib.auth.mixins import UserPassesTestMixin,LoginRequiredMixin
 
 class BaseManageView(APIView):
     """
@@ -25,15 +25,17 @@ class BaseManageView(APIView):
 class ProjectDetailsView(generics.RetrieveAPIView):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (permissions.AllowAny,)
 
-class ProjectUpdateView(UserPassesTestMixin,generics.UpdateAPIView):
+class ProjectUpdateView(LoginRequiredMixin,UserPassesTestMixin,generics.UpdateAPIView):
+    raise_exception = True
     def test_func(self):
         return (self.get_object().client_id == self.request.user.pk)
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
 
 class ProjectDestroyView(UserPassesTestMixin,generics.DestroyAPIView):
+    raise_exception = True
     def test_func(self):
         return (self.get_object().client_id == self.request.user.pk)
     queryset = Project.objects.all()
@@ -49,7 +51,9 @@ class ProjectManageView(BaseManageView):
     }
 
 
-class ProjectCreateView(UserPassesTestMixin, generics.CreateAPIView):
+
+class ProjectCreateView(LoginRequiredMixin,UserPassesTestMixin, generics.CreateAPIView):
+    raise_exception = True
     def test_func(self):
         return self.request.user.is_client
 
@@ -59,15 +63,19 @@ class ProjectCreateView(UserPassesTestMixin, generics.CreateAPIView):
         serializer.save(client=self.request.user)
         serializer.save(status='active')
 
-
-class ProjectListView(generics.ListAPIView):
+class MyProjectsView(generics.ListAPIView):
     serializer_class = ProjectSerializer
     def get_queryset(self):
         user = self.request.user
         return Project.objects.filter(client_id = user.pk)
 
-
-class HomepageView(generics.ListAPIView):
+class ProjectListView(generics.ListAPIView):
     serializer_class = ProjectSerializer
     queryset = Project.objects.all().order_by('-id')
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (permissions.AllowAny,)
+
+class ProjectListManageView(BaseManageView):
+    VIEWS_BY_METHOD = {
+        'POST': ProjectCreateView.as_view,
+        'GET' : ProjectListView.as_view
+    }
