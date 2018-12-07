7943d8a7914b7370f575a974e5348e51b3bb4c39
Umut Barış Öztunç <umutoztunc@gmail.com>
2018-12-07 13:40:40 -0500

    Created an endpoint to search users by name.

--- Project/backend/user/urls.py
+++ Project/backend/user/urls.py
@@ -2,11 +2,12 @@
 from django.contrib import admin
 from rest_auth.views import UserDetailsView
 from django.urls import path, include, re_path
-from .views import LogoutViewEx, UserRetrieveView, UserListView
+from .views import LogoutViewEx, UserRetrieveView, UserListView, UserSearchView
 
 # URL patterns of the URL dispatcher of User app.
 urlpatterns = [
     path('self/', UserDetailsView.as_view(), name='user_self'),
     path('<int:pk>/', UserRetrieveView.as_view(), name='user_retrieve'),
     path('', UserListView.as_view(), name='user_list'),
+    path('search/<str:name>/', UserSearchView.as_view(), name='user_search'),
 ]
--- Project/backend/user/views.py
+++ Project/backend/user/views.py
@@ -4,6 +4,7 @@ from rest_auth.views import LogoutView
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 from .models import User
 from .serializers import UserSerializer
+from django.db.models import Q
 
 # Extended the LogoutView from REST Auth module to make it only accessible by authenticated requests.
 # Added the custom ProjectViews using APIView's
@@ -29,4 +30,10 @@ class UserListView(ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-        
+class UserSearchView(ListAPIView):
+    permision_classes = (AllowAny,)
+    serializer_class = UserSerializer
+    
+    def get_queryset(self):
+        name = self.kwargs['name']
+        return User.objects.filter(Q(first_name__icontains=name) | Q(last_name__icontains=name))
